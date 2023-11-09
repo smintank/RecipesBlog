3d8b66eda366136b27e11bb50a72eb086d64ad0a
smintank <assbestmail@gmail.com>
2023-11-09 21:05:19 +0300

    Set up admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Recipe, Ingredient, Tag
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Tag)
