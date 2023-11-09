597486c216220d70a6487af4105f6c6f17fa28b0
smintank <assbestmail@gmail.com>
2023-11-09 21:07:56 +0300

    Add in settings:
    - Rest framework settings
    - Static and media files settings
    - Language to Ru
    - Add apps
    - Settings of localhost

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,8 +11,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-o4w5+%^rrtm7=d11)a
 
 DEBUG = bool(int(os.getenv("DEBUG", default=0)))
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(' ')
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,6 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework.authtoken',
+    'rest_framework',
+    'djoser',
+    'recipes.apps.RecipesConfig'
 ]
 
 MIDDLEWARE = [
@@ -86,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
@@ -94,13 +99,25 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+}
