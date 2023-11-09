308c22d0bc4f51fa47848fa41c6883f2057b7443
smintank <assbestmail@gmail.com>
2023-11-09 21:08:50 +0300

    Add RecipeViewSet in views.py

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from recipes.models import Recipe
+from recipes.serializer import RecipeSerializer
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    pagination_class = PageNumberPagination
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
