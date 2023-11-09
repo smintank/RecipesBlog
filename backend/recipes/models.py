8629c3585a7fc4e10bc93f6842ade33276929bfb
smintank <assbestmail@gmail.com>
2023-11-09 21:03:39 +0300

    Add models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,77 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя',
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цвет',
+    )
+    slug = models.SlugField(
+        max_length=200,
+        verbose_name='Slug',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    author = models.ForeignKey(
+        User, related_name='recipes',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    text = models.TextField(verbose_name='Описание')
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        null=True,
+        default=None,
+        verbose_name='Изображение'
+    )
+    ingredient = models.ManyToManyField(
+        Ingredient, verbose_name='Ингридиенты',
+        related_name='ingredient_id'
+    )
+    tags = models.ManyToManyField(
+        Tag, verbose_name='Теги',
+        related_name='tag_id'
+    )
+    cooking_time = models.IntegerField(
+        verbose_name='Время приготовления'
+    )
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
