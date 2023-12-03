4ecfc435e04d0de5b6d3131e923764256e7c32d2
smintank <assbestmail@gmail.com>
2023-12-03 15:31:01 +0300

    Refactor is_favorited is_in_shopping_cart filters

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -13,21 +13,23 @@ class RecipeFilter(FilterSet):
         to_field_name='slug'
 
     )
-    is_favorited = BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = BooleanFilter(method='get_is_in_shopping_cart')
+    is_favorited = CharFilter(method='get_is_favorited')
+    is_in_shopping_cart = CharFilter(method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_in_shopping_cart', 'is_favorited')
 
     def get_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
+        user = self.request.user
+        if value.lower() in ('1', 'true') and user.is_authenticated:
+            return queryset.filter(favorites__user=user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(shopping_cart__user=self.request.user)
+        user = self.request.user
+        if value.lower() in ('1', 'true') and user.is_authenticated:
+            return queryset.filter(shopping_cart__user=user)
         return queryset
 
 
