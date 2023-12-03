dd96f6ba6beae6e1ef0210779a736c30dbacece5
smintank <assbestmail@gmail.com>
2023-12-03 15:23:20 +0300

    Refactor shopping_cart pdf download view

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,5 +1,7 @@
 import io
 
+from django.db.models import IntegerField, F
+from django.db.models.functions import Cast
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
@@ -132,67 +134,79 @@ class ShoppingCartView(FavoriteView):
 
 
 class DownloadCartView(views.APIView):
-    title_font_size = 16
-    text_font_size = 12
-    title_x_y = 230, 800
-    ingredient_y = 760
-    ingredient_x = 70
-    amount_x = 450
-    y_shift = 25
-    file_name = 'groceries.pdf'
+    SETTINGS = {
+        'file_name': 'groceries.pdf',
+        'title_text': 'Список покупок',
+        'title_x_y': (230, 800),
+        'font': 'Roboto-Regular',
+        'font_path': 'Roboto-Regular.ttf',
+        'title_font_size': 16,
+        'text_font_size': 12,
+        'ingredient_x': 70,
+        'amount_x': 450,
+        'row_start_y': 760,
+        'row_shift_y': 25,
+    }
 
     def get(self, request, *args, **kwargs):
         buffer = io.BytesIO()
-
-        response = self._set_settings()
-        ingredient_sum = self._get_ingredients_data()
+        response = self._set_settings(
+            filename=self.SETTINGS['file_name'],
+            font=self.SETTINGS['font'],
+            font_path=self.SETTINGS['font_path']
+        )
+        grocery_list = self._get_grocery_list(user_id=self.request.user.id)
 
         page = canvas.Canvas(response)
-        self._fill_page(ingredient_sum, page)
+        self._fill_page(
+            grocery_list=grocery_list, page=page, settings=self.SETTINGS
+        )
         page.showPage()
         page.save()
 
         buffer.seek(0)
         return response
 
-    def _fill_page(self, ingredient_sum, page):
-        page.setFont('Roboto-Regular', self.title_font_size)
-        page.drawString(*self.title_x_y, text='Список покупок')
-        page.setFont('Roboto-Regular', self.text_font_size)
-        height = self.ingredient_y
-        for i, ingredient in enumerate(ingredient_sum):
-            height -= self.y_shift
-            name = ingredient['name'].capitalize()
-            page.drawString(self.ingredient_x, height, f'{i + 1}. {name}')
+    @staticmethod
+    def _fill_page(grocery_list, page, settings):
+        page.setFont(settings['font'], settings['title_font_size'])
+        page.drawString(*settings['title_x_y'], text=settings['title_text'])
+        page.setFont(settings['font'], settings['text_font_size'])
+        row_y = settings['row_start_y']
 
-            amount = ingredient['amount']
-            unit = ingredient['unit']
-            page.drawString(self.amount_x, height, f'{amount} {unit}')
+        for i, item in enumerate(grocery_list, start=1):
+            row_y -= settings['row_shift_y']
+            name = item['name'].capitalize()
+            amount = item['amount']
+            unit = item['measurement_unit']
 
-    def _get_ingredients_data(self):
+            page.drawString(settings['ingredient_x'], row_y, f'{i}. {name}')
+            page.drawString(settings['amount_x'], row_y, f'{amount} {unit}')
+
+    @staticmethod
+    def _get_grocery_list(user_id):
         ingredient_set = RecipeIngredient.objects.values(
-            'ingredient__name', 'amount', 'ingredient__measurement_unit'
+            name=F('ingredient__name'),
+            measurement_unit=F('ingredient__measurement_unit')
+        ).annotate(
+            amount=Cast('amount', IntegerField())
         ).filter(
-            recipe__shopping_cart__user=self.request.user.id
-        ).order_by('ingredient__name')
+            recipe__shopping_cart__user=user_id
+        ).order_by('name')
 
-        current_ingredient = ''
         ingredient_sum = []
         for item in ingredient_set:
-            if item['ingredient__name'] != current_ingredient:
-                current_ingredient = item['ingredient__name']
-                ingredient_sum.append({
-                    'amount': int(item['amount']),
-                    'name': item['ingredient__name'],
-                    'unit': item['ingredient__measurement_unit']})
+            if ingredient_sum and item['name'] == ingredient_sum[-1]['name']:
+                ingredient_sum[-1]['amount'] += item['amount']
             else:
-                ingredient_sum[-1]['amount'] += int(item['amount'])
+                ingredient_sum.append(item)
         return ingredient_sum
 
-    def _set_settings(self):
-        pdfmetrics.registerFont(TTFont('Roboto-Regular', 'Roboto-Regular.ttf'))
+    @staticmethod
+    def _set_settings(filename, font=None, font_path=None):
+        if font and font_path:
+            pdfmetrics.registerFont(TTFont(font, font_path))
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; ' \
-                                          f'filename="{self.file_name}"'
-
+        content_disposition = f'attachment; filename="{filename}"'
+        response['Content-Disposition'] = content_disposition
         return response
